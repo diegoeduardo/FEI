ca752c865a2b0c355f61fb1e6a1deef58e59a6bd
DIEGOEDUARDOSILVA <silva.diegoedu@gmail.com>
2019-06-18 21:14:30 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -4,18 +4,29 @@
 int const MAXTAM=1000;
 
 int Frente, Tras;
-int Fila[MAXTAM];
-
+int FilaDados[MAXTAM];
+//Classe Fila
+class Fila{
+    public:
+    void FilaConstrutor();
+    bool ChecaFilaVazia();
+    bool ChecaFilaCheia();
+    bool EnfileirarFila(int valor);
+    bool DesenfileirarFila(int &valor);
+    bool ConsultaFrenteFila(int &valor);
+    int TamanhoFila();
+    
+};
 //posicao inicial
 //quando removemos, removemos do inicio
 //quando inserimos vamos inserir no fim
-void FilaConstrutor(){
+void Fila::FilaConstrutor(){
     Frente=0;
     Tras=-1;
 }
 //a fila estara vazia quando a frente estara igual a tras
 //frente tem que ser maior que tras
-bool ChecaFilaVazia(){
+bool Fila::ChecaFilaVazia(){
     if(Frente>Tras){
         return true;
     }else{
@@ -25,7 +36,7 @@ bool ChecaFilaVazia(){
     //return Frente>Tras;
 }
 //a fila estara cheia quando o tras = MAXTAM-1 e nao pode ser inserido mais nenhum valor
-bool ChecaFilaCheia(){
+bool Fila::ChecaFilaCheia(){
     if(Tras==MAXTAM-1){
         return true;
     }else{
@@ -34,59 +45,60 @@ bool ChecaFilaCheia(){
 }
 //checa se a fila esta cheia, estando nao posso inserir
 //se a fila nao estiver cheia, enfilero um novo valor(insiro um novo valor)
-bool EnfileirarFila(int valor){
+bool Fila::EnfileirarFila(int valor){
     if(ChecaFilaCheia()){
         return false;
     }else{
         Tras++;
-        Fila[Tras]=valor;
+        FilaDados[Tras]=valor;
         return true;
     }
 }
 //checa se a fila esta vazia, estando nao posso desenfilerar
 //se a fila nao estiver vazia, desenfilero um valor(retiro um valor)
-bool DesenfileirarFila(int &valor){
+bool Fila::DesenfileirarFila(int &valor){
     if(ChecaFilaVazia()){
         return false;
     }else{
-        valor=Fila[Frente];
+        valor=FilaDados[Frente];
         Frente++;
         return true;
     }
 }
 //obteremos a frete da fila mas nao incrementaremos ela 
-bool ConsultaFrenteFila(int &valor){
+bool Fila::ConsultaFrenteFila(int &valor){
     if(ChecaFilaVazia()){
         return false;
     }else{
-        valor=Fila[Frente];
+        valor=FilaDados[Frente];
         return true;
     }
 }
 //para buscar o tamanho temos o tras - frete + 1 
 // esse mais um é devido a estarmos contando a partir do -1 , posicao do tras
-int TamanhoFila(){
+int Fila::TamanhoFila(){
     return (Tras - Frente)+1;
 }
 
 
 int main(){
     int Valor;
-    FilaConstrutor();
+    //FilaConstrutor();
+    Fila fila;
+    fila.FilaConstrutor();
+    fila.EnfileirarFila(5);
+    fila.EnfileirarFila(2);
+    fila.EnfileirarFila(7);
 
-    EnfileirarFila(5);
-    EnfileirarFila(2);
-    EnfileirarFila(7);
-
-    printf("\n\nTamanho:%d\n\n",TamanhoFila());
+    printf("\n\nTamanho:%d\n\n",fila.TamanhoFila());
 
 
-    while( DesenfileirarFila(Valor) ){
+    while( fila.DesenfileirarFila(Valor) ){
         printf("\nValor:%d\n",Valor);
     }
-    printf("\n\nTamanho:%d\n\n",TamanhoFila());
+    printf("\n\nTamanho:%d\n\n",fila.TamanhoFila());
     
-	ConsultaFrenteFila(valor);
+	fila.ConsultaFrenteFila(Valor);
     /*
     if( DesenfileirarFila(Valor) ){
         printf("\n\nValor:%d\n\n",Valor);
